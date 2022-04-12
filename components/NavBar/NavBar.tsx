8e7b451a0051ce735fd8624b8e9da89a820ca6ea
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-04-12 18:55:54 +0200

    actually use da thing

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -28,12 +28,16 @@ export default function NavBar(props: NavBarProps) {
                     <div className="absolute inset-y-0 left-0 flex items-center">
                         <button type="button" onClick={_ => setDroppedDown(!droppedDown)} className="inline-flex items-center justify-center p-2 rounded-md hover:bg-gray-900" aria-controls="mobile-menu" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
-                            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                            </svg>
-                            <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                            </svg>
+                            {droppedDown ?
+                                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                                :
+
+                                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                                </svg>
+                            }
                         </button>
                     </div>
                 </div>
@@ -41,27 +45,27 @@ export default function NavBar(props: NavBarProps) {
             {shouldDisplay ?
                 <div className="sm:container sm:px-4 sm:py-2 sm:flex sm:flex-wrap sm:justify-around sm:items-center sm:mx-auto">
                     <div className="px-2 pb-3 sm:p-0 sm:py-2 sm:m-0 sm:items-center sm:space-x-16 sm:flex">
-                        <Anime easing={'easeOutElastic(1, .8)'} translateY={[-50, 0]} opacity={[0, 1]} delay={ delay += delayInc }>
+                        <Anime easing={'easeOutElastic(1, .8)'} translateY={[-50, 0]} opacity={[0, 1]} delay={delay += delayInc}>
                             <Link href="/">
                                 <a className="hover:text-purple-300 hover:underline block px-3 py-2 sm:flex sm:p-0" title="the landing page">home</a>
                             </Link>
                         </Anime>
-                        <Anime easing={'easeOutElastic(1, .8)'} translateY={[-50, 0]} opacity={[0, 1]} delay={ delay += delayInc }>
+                        <Anime easing={'easeOutElastic(1, .8)'} translateY={[-50, 0]} opacity={[0, 1]} delay={delay += delayInc}>
                             <Link href="/about/">
                                 <a className="hover:text-purple-300 hover:underline block px-3 py-2 sm:flex sm:p-0" title="cool info">about me</a>
                             </Link>
                         </Anime>
-                        <Anime easing={'easeOutElastic(1, .8)'} translateY={[-50, 0]} opacity={[0, 1]} delay={ delay += delayInc }>
+                        <Anime easing={'easeOutElastic(1, .8)'} translateY={[-50, 0]} opacity={[0, 1]} delay={delay += delayInc}>
                             <Link href="/projects/">
                                 <a className="hover:text-purple-300 hover:underline block px-3 py-2 sm:flex sm:p-0" title="some of my work">projects</a>
                             </Link>
                         </Anime>
-                        <Anime easing={'easeOutElastic(1, .8)'} translateY={[-50, 0]} opacity={[0, 1]} delay={ delay += delayInc }>
+                        <Anime easing={'easeOutElastic(1, .8)'} translateY={[-50, 0]} opacity={[0, 1]} delay={delay += delayInc}>
                             <Link href="/posts/">
                                 <a className="hover:text-purple-300 hover:underline block px-3 py-2 sm:flex sm:p-0" title="articles about random topics">posts</a>
                             </Link>
                         </Anime>
-                        <Anime easing={'easeOutElastic(1, .8)'} translateY={[-50, 0]} opacity={[0, 1]} delay={ delay += delayInc }>
+                        <Anime easing={'easeOutElastic(1, .8)'} translateY={[-50, 0]} opacity={[0, 1]} delay={delay += delayInc}>
                             <Link href="/contact/">
                                 <a className="hover:text-purple-300 hover:underline block px-3 py-2 sm:flex sm:p-0" title="my socials, pgp, & email">contact me</a>
                             </Link>
