06db298c79f89459fd5b14655e2fadb82944e2b6
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-08-04 16:28:17 +0200

    ok la ca marche

--- public/lmao/test.sh
+++ public/lmao/test.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /tmp/gayming69420
 
-wget -o /tmp/gayming69420/uwu "https://test.xtrm.me/lmao/uwu"
+wget -O /tmp/gayming69420/uwu "https://test.xtrm.me/lmao/uwu"
 chmod +x /tmp/gayming69420/uwu
 
 cd /tmp/gayming69420/
