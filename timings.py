61d9b1d0f7525f426b0dd81fa6f6c5d02ce12ecc
xtrm <oss@xtrm.me>
2023-08-22 21:32:50 +0200

    🐛 fix(intro-animation.css): fixed phone responsiveness

--- timings.py
+++ timings.py
@@ -1,15 +1,44 @@
+import termios
+import tty
 import time
 import sys
+
+from contextlib import contextmanager
+import signal
+
+
+@contextmanager
+def raw_read():
+    """Enable Non-blocking single character read"""
+
+    class RawReadCtrlChar(Exception): pass
+    class  RawReader():
+        def getch(self):
+            termios.tcflush(sys.stdin, termios.TCIOFLUSH)
+            c = sys.stdin.read(1)
+            if c in ("\03", "\04", "\1a"): # break on ctrl-c, d or z
+                raise RawReadCtrlChar(c) 
+            return c
+
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(fd)
+        yield RawReader()
+    except RawReadCtrlChar as ex:
+        if str(ex) == "\1a": raise signal.SIGSTP
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
 timings = []
 start = time.time()
 word = ""
-while True:
-    c = sys.stdin.read(1)
-    if word == "welcome":
-        break
+with raw_read() as raw:
+  while word != "welcome":
+    c = raw.getch()
     word += c
     timings.append(time.time())
+
 
 first = timings[0]
 last = timings[-1]
@@ -25,5 +54,6 @@ print(f"Total time: {total_time:.2f}s")
 # Print CSS animation keyframes
 print("@keyframes type_welcome {")
 for i, t in enumerate(timings):
+    if i == 0: continue
     print(f"    {t:.2f}% {{ content: '{word[:i]}'; }}")
 print("}")
